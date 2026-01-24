6249765f318888cccc370b110d4d4f94581dfe92
Tejus Jaiswal <tejusjaiswal13@gmail.com>
2026-01-24 18:36:17 +0530

    Create SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+
+    // State variable to store a number
+    uint256 public storedNumber;
+
+    // Function to store a number
+    function setNumber(uint256 _number) public {
+        storedNumber = _number;
+    }
+
+    // Function to get the stored number
+    function getNumber() public view returns (uint256) {
+        return storedNumber;
+    }
+}
