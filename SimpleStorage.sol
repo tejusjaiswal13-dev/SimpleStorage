c54b1159c28657d466124cebe0a5af1dce08842e
Tejus Jaiswal <tejusjaiswal13@gmail.com>
2026-01-31 21:13:54 +0530

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,18 +1,39 @@
+// I'm a comment!
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+
+pragma solidity 0.8.19;
+
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
 
 contract SimpleStorage {
+    uint256 myFavoriteNumber;
 
-    // State variable to store a number
-    uint256 public storedNumber;
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+    // uint256[] public anArray;
+    Person[] public listOfPeople;
 
-    // Function to store a number
-    function setNumber(uint256 _number) public {
-        storedNumber = _number;
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public virtual {
+        myFavoriteNumber = _favoriteNumber;
     }
 
-    // Function to get the stored number
-    function getNumber() public view returns (uint256) {
-        return storedNumber;
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
+
+contract SimpleStorage2 {}
+
+contract SimpleStorage3 {}
+
+contract SimpleStorage4 {}
